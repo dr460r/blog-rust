7f16cb69204f2cdf6e4e60ea4162a7baf0f43259
Dragor Draganović <dragor.draganovic@gmail.com>
2024-11-01 16:15:40 +0100

    feat: add new blog post model + fix
    - Add model, implement transformation.
    - Fix Handler trait not implemented (T for Json wasn't Deserializable)

--- src/models.rs
+++ src/models.rs
@@ -1,12 +1,31 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
-#[derive(Debug, FromRow, Serialize)]
+#[derive(Debug, FromRow, Serialize, Deserialize)]
 pub struct BlogPost {
-    id: i32,
-    text: String,
-    post_date: String,
-    user_name: String,
-    image_path: String,
-    avatar_path: String,
+    pub text: String,
+    pub post_date: String,
+    pub user_name: String,
+    pub image_path: String,
+    pub avatar_path: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewBlogPost {
+    pub text: String,
+    pub image: String,
+    pub user_name: String,
+    pub avatar_url: String,
+}
+
+impl NewBlogPost {
+    pub fn to_db_model(self) -> BlogPost {
+        BlogPost {
+            text: self.text,
+            post_date: "".to_string(),
+            user_name: "".to_string(),
+            image_path: "".to_string(),
+            avatar_path: "".to_string(),
+        }
+    }
 }
